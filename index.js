1eca881447f6a9045461e6c6c434df700350b33b
BerendKemper <46784771+BerendKemper@users.noreply.github.com>
2020-07-27 17:29:59 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+"use strict";
+const { readdir, stat, Stats } = require("fs");
+const { join } = require("path");
+
+const allKeysStats = {};
+for (const key of Object.keys(new Stats()))
+  allKeysStats[key] = true;
+
+exports = (basePath, keysStats = []) => {
+  return new Promise((resolve, reject) => {
+    if (keysStats instanceof Array) {
+      const baseTree = {};
+      (function checkKeys() {
+        const cloneKeys = Array.from(keysStats);
+        keysStats = [];
+        for (const key of cloneKeys)
+          if (allKeysStats[key] !== undefined)
+            keysStats.push(key);
+      }());
+      const subTree = (subPath, objSubTree) => {
+        return new Promise((resolve, reject) => {
+          _fs.stat(subPath, (err, stats) => {
+            if (err !== null)
+              reject();
+            if (stats.isDirectory()) {
+              _fs.readdir(subPath, async (err, files) => {
+                for (const file of files) {
+                  objSubTree[file] = {};
+                  await subTree(subPath + "/" + file, objSubTree[file]);
+                }
+                resolve();
+              });
+            }
+            else if (stats.isFile()) {
+              for (const key of keysStats)
+                objSubTree[key] = stats[key];
+              resolve();
+            }
+          });
+        });
+      };
+      subTree(basePath, baseTree)
+        .then(() => resolve(baseTree))
+        .catch(err => reject());
+    }
+    else
+      reject("statsParams must be an Array");
+  });
+};
