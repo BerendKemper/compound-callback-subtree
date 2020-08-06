54ed7e424c1a0303b04c7f46b58cd913f496600d
BerendKemper <46784771+BerendKemper@users.noreply.github.com>
2020-08-06 16:20:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,41 @@ const { join } = require("path");
 
 const allKeysStats = {};
 for (const key of Object.keys(new Stats()))
-  allKeysStats[key] = true;
+    allKeysStats[key] = true;
+
+const subTree = (subPath, statCallbacks) => {
+    const baseTree = {};
+    const subTree = (subPath, objSubTree) => {
+        return new Promise((resolve, reject) => {
+            _fs.stat(subPath, (err, stats) => {
+                if (err !== null)
+                    reject();
+                if (stats.isDirectory()) {
+                    _fs.readdir(subPath, async (err, files) => {
+                        for (const file of files) {
+                            const nextPath = _path.join(subPath, file);
+                            if (file.endsWith(".json") && subPath.endsWith(file.substring(0, file.length - 5)))
+                                Object.assign(objSubTree, await configJson(nextPath));
+                            else {
+                                objSubTree[file] = {};
+                                await subTree(nextPath, objSubTree[file]);
+                            }
+                        }
+                        resolve(baseTree);
+                    });
+                }
+                else if (stats.isFile()) {
+                    for (const key in statCallbacks) {
+                        const statCallback = statCallbacks[key];
+                        objSubTree[statCallback.key || key] = statCallback(stats[key]);
+                    }
+                    resolve(baseTree);
+                }
+            });
+        });
+    };
+    return subTree(subPath, baseTree);
+};
 
 /**
  * Create a tree of sub-directories and show stats of files
@@ -13,47 +47,22 @@ for (const key of Object.keys(new Stats()))
  * @return {Object}                 Tree with stats-values that can be modified by the statsCallbacks
  */
 const fsTreeStats = (basePath, statsCallbacks = {}) => {
-  return new Promise((resolve, reject) => {
-    if (typeof statsCallbacks === "object" && statsCallbacks instanceof Array === false) {
-      const baseTree = {};
-      (function checkKeys() {
-        const keys = Object.keys(statsCallbacks);
-        for (const key of keys)
-          if (allKeysStats[key] === undefined || typeof statsCallbacks[key] !== "function")
-            delete(statsCallbacks[key]);
-      }());
-      const subTree = (subPath, objSubTree) => {
-        return new Promise((resolve, reject) => {
-          stat(subPath, (err, stats) => {
-            if (err !== null)
-              reject();
-            if (stats.isDirectory()) {
-              readdir(subPath, async (err, files) => {
-                for (const file of files) {
-                  objSubTree[file] = {};
-                  await subTree(join(subPath, file), objSubTree[file]);
-                }
-                resolve();
-              });
-            }
-            else if (stats.isFile()) {
-              for (const key in statsCallbacks)
-                objSubTree[statsCallbacks[key].name || key] = statsCallbacks[key](stats[key]);
-              resolve();
-            }
-          });
-        });
-      };
-      subTree(basePath, baseTree)
-        .then(() => resolve(baseTree))
-        .catch(err => reject());
-    }
-    else
-      reject("statsParams must be an Array");
-  });
+    return new Promise((resolve, reject) => {
+        if (typeof statsCallbacks === "object" && statsCallbacks instanceof Array === false) {
+            const baseTree = {};
+            (function checkKeys() {
+                const keys = Object.keys(statsCallbacks);
+                for (const key of keys)
+                    if (allKeysStats[key] === undefined || typeof statsCallbacks[key] !== "function")
+                        delete(statsCallbacks[key]);
+            }());
+            subTree(basePath, baseTree)
+                .then(() => resolve(baseTree))
+                .catch(err => reject());
+        }
+        else
+            reject("statsParams must be an Array");
+    });
 };
-const namedStatsCallback = (name, statsCallback) => {
-  return Object.defineProperty(statsCallback, 'name', { value: name });
-};
-module.exports.fsTreeStats = fsTreeStats;
-module.exports.namedStatsCallback = namedStatsCallback;
+const namedStatsCallback = (name, statsCallback) =>  statsCallback.key = name;
+module.exports = Object.freeze({ fsTreeStats, namedStatsCallback });
