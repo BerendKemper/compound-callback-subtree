336cb782439f3897d24d4dbd9eec5181c12f384e
Berend Kemper <berend_kemper@hotmail.com>
2020-10-01 12:01:31 +0200

    v3.0.0

--- index.js
+++ index.js
@@ -1,43 +1,86 @@
 "use strict";
 const fs = require("fs");
 const _path = require("path");
-const { timeStamp } = require("console");
-const _subTree = new WeakMap();
+function PrivateTree(options) {
+	this.dirStatsCb = options.dirStatsCb || ((data, callback) => callback());
+	this.fileStatsCb = options.fileStatsCb || ((data, callback) => callback());
+	this.subBranchCb = options.subBranchCb || ((data, nextBranch, blockBranch) => nextBranch());
+	this.cacheQueue = [];
+};
+PrivateTree.prototype.fromPath = function fromPath(path, callback) {
+	const branch = {};
+	this.counter = 1;
+	this.basepath = path;
+	this.branchData = { [path]: { path, branch } };
+	this.returnTree = () => callback(null, branch);
+	this.callback = callback;
+	this.subBranch(path);
+};
+PrivateTree.prototype.subBranch = function subBranch(path) {
+	fs.stat(path, (error, stats) => this.onStat(error, stats, path));
+};
+PrivateTree.prototype.onStat = function onStat(error, stats, path) {
+	if (error !== null)
+		this.callback(error);
+	this.branchData[path].stats = stats
+	if (stats.isDirectory())
+		this.dirStatsCb(this.branchData[path], () => this.onDirStatsCb(path));
+	else if (stats.isFile())
+		this.fileStatsCb(this.branchData[path], () => this.checkTreeFinished());
+};
+PrivateTree.prototype.onDirStatsCb = function onDirStatsCb(dirpath) {
+	fs.readdir(dirpath, (error, files) => this.onReaddir(error, files, dirpath));
+};
+PrivateTree.prototype.onReaddir = function onReaddir(error, files, dirpath) {
+	if (error !== null)
+		this.callback(error);
+	this.counter--;
+	for (const file of files) {
+		this.counter++;
+		const path = _path.join(dirpath, file);
+		this.branchData[path] = { path, dirpath, file, dirbranch: this.branchData[dirpath].branch };
+		this.subBranchCb(this.branchData[path], nextbranch => this.onSubBranchCb(path, file, nextbranch), () => this.checkTreeFinished());
+	};
+};
+PrivateTree.prototype.onSubBranchCb = function onSubBranchCb(path, file, nextbranch) {
+	const branch = this.branchData[path].dirbranch[file] = nextbranch || {};
+	this.branchData[path].branch = branch;
+	this.subBranch(path);
+};
+PrivateTree.prototype.checkTreeFinished = function checkTreeFinished() {
+	if (--this.counter === 0) {
+		this.returnTree();
+		this.clearCache();
+	}
+};
+PrivateTree.prototype.clearCache = function clearCache() {
+	const branch = this.branchData[this.basepath].branch;
+	for (const callback of this.cacheQueue)
+		callback(branch);
+	this.cacheQueue = [];
+};
+PrivateTree.prototype.fromCache = function fromCache(callback) {
+	if (this.counter === undefined)
+		callback(new Error("nothing in cache..."));
+	if (this.counter === 0)
+		callback(null, this.branchData[this.basepath].branch);
+	else
+		this.cacheQueue.push(callback);
+};
 class CompoundCallbackSubTree {
-    /**@param {Object} options 
-    *  @param {Function} options.dirStatsCb
-    *  @param {Function} options.fileStatsCb
-    *  @param {Function} options.subBranchCb */
-    constructor(options = {}) {
-        const { dirStatsCb = (data, callback) => callback(), fileStatsCb = (data, callback) => callback(), subBranchCb = data => new Promise(resolve => resolve()) } = options;
-        _subTree.set(this, function subTree(subPath, branch) {
-            return new Promise((resolve, reject) =>
-                fs.stat(subPath, (err, stats) => {
-                    if (err !== null)
-                        reject("fs.stats() cought an error");
-                    if (stats.isDirectory())
-                        dirStatsCb({ path: subPath, stats, branch }, () =>
-                            fs.readdir(subPath, async (err, files) => {
-                                if (err !== null)
-                                    reject("fs.readdir() cought an error");
-                                for (const file of files) {
-                                    const nextPath = _path.join(subPath, file);
-                                    await new Promise(resolve => subBranchCb({ path: subPath, nextPath, file, branch })
-                                        .then(nextBranch => subTree(nextPath, branch[file] = nextBranch || {}).then(() => resolve()))
-                                        .catch(() => resolve()));
-                                }
-                                resolve();
-                            }));
-                    else if (stats.isFile())
-                        fileStatsCb({ path: subPath, stats, branch }, () => resolve());
-                }));
-        });
-    }
-    /**@param {String} basePath
-     * @param {Function} callback Default: tree => console.log(tree) */
-    subTree(basePath, callback = tree => console.log(tree)) {
-        const tree = {};
-        _subTree.get(this)(basePath, tree).then(() => callback(tree));
-    }
+	/**@param {Object} options 
+	*  @param {Function} options.dirStatsCb
+	*  @param {Function} options.fileStatsCb
+	*  @param {Function} options.subBranchCb */
+	constructor(options = {}) {
+		const privateTree = new PrivateTree(options);
+		this.fromPath = function fromPath(basePath, callback = (error, tree) => console.log(tree)) {
+			return privateTree.fromPath(basePath, callback);
+		};
+		this.fromCache = function fromCache(callback = (error, tree) => console.log(tree)) {
+			privateTree.fromCache(callback);
+		};
+		Object.freeze(this);
+	}
 };
 module.exports = CompoundCallbackSubTree;
